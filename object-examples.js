049f72ef6cc47fe016c732b451abb535ef049fb8
Christine Rowland <fordo29@gmail.com>
2021-09-21 22:11:48 -0600

    Add car Object model

---
+++ object-examples.js
@@ -0,0 +1,11 @@
+var car = {
+  brand: "Toyota",
+  bodyType: "Minivan",
+  color: "Green",
+  numberOfDoors: 4,
+  parkAssist: false,
+  sunRoof: false,
+  dvdPlayer: true,
+  yearMade: 2019,
+  price: 19000
+}
