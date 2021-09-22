7e6bc1a4b8de4befcacb5ccfecd08552e435c67a
Christine Rowland <fordo29@gmail.com>
2021-09-21 22:25:41 -0600

    Add Animal object Model

--- object-examples.js
+++ object-examples.js
@@ -7,5 +7,14 @@ var car = {
   sunRoof: false,
   dvdPlayer: true,
   yearMade: 2019,
-  price: 19000
-}
+  price: 19000,
+};
+
+var bird = {
+  species: "Parrot",
+  diet:["seeds", "nuts", "fruit"],
+  intelligence: true,
+  pet: true,
+  age: 17,
+  ageExpectancy: 65,
+};
